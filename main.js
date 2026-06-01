d6830a5be40f36045c0cd5770956a5297691c7b7
sanemondhuynh12-cmd <sanemond.huynh12@gmail.com>
2026-06-01 06:08:22 +0000

    Calculate bill total

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+function calculateBillTotal(TotalMeal){
+    Tax = TotalMeal * 0.095;
+    Tip = TotalMeal * 0.2;
+    Total = Tax + Tip;
+    TotalCost = TotalMeal + Tax + Tip
+    return(TotalCost);
+}
 
+console.log(calculateBillTotal(50));
